4037a2df1f8b1a44626cd0bdf5c946be76dd489d
Antonov333 <antonovsergei333@yahoo.com>
2023-07-25 15:32:59 +0300

    MathQuestionController endpoints created and methods implemented

--- src/main/java/pro/sky/java/course2/MathQuestionController.java
+++ src/main/java/pro/sky/java/course2/MathQuestionController.java
@@ -2,6 +2,7 @@ package pro.sky.java.course2;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Set;
@@ -19,5 +20,17 @@ public class MathQuestionController {
     @GetMapping(path = "/math/")
     Set<Question> getAll() {
         return mathQuestionService.getAll();
+    }
+
+    @GetMapping(path = "/math/add/")
+    Question add(@RequestParam(required = false, name = "problem") String problem,
+                 @RequestParam(required = false, name = "answer") String answer) {
+        return mathQuestionService.add(problem, answer);
+    }
+
+    @GetMapping(path = "/math/remove/")
+    Question remove(@RequestParam(required = false, name = "problem") String problem,
+                    @RequestParam(required = false, name = "answer") String answer) {
+        return mathQuestionService.removeQuestion(problem, answer);
     }
 }
